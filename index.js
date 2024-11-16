388fee1d63d51d19b19f35aab9bdd93675e6aef6
Sah-Abhishek <abhisheksah3333@gmail.com>
2024-11-16 17:22:10 +0530

    installed cors

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.status(200).json({message: "server is running"});
