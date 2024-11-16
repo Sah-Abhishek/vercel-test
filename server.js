f442ba5bc96f0071696612c320d0da94f8b04154
Sah-Abhishek <abhisheksah3333@gmail.com>
2024-11-16 17:11:51 +0530

    Changed to type module

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 
 app.use("/", (req, res) => {
-    res.send("server is running")
+    res.status(200).json({message: "server is running"});
 })
 
 app.listen(5000, console.log("Server started on port 5000"));
