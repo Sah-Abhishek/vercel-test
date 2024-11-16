52222c617d61e34fc1ee931eb9941847f672a098
Sah-Abhishek <abhisheksah3333@gmail.com>
2024-11-16 18:15:49 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
-import express from 'express';
-import cors from 'cors';
+// import express from 'express';
+// import cors from 'cors';
+// const app = express();
+// app.use(express.json());
+// app.use(cors({
+//     origin: ["https://vishal-kr.vercel.app/"],
+//     methods: ["POST", "GET"],
+//     credentilas: true
+// }));
+
+// app.get("/", (req, res) => {
+//     res.status(200).json({ message: "server is running" });
+// })
+
+// app.listen(5000, console.log("Server started on port 5000"));
+
+
+
+
+import express from "express";
+import dotev from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import HistoryRoute from "./routes/HistoryRoute.js";
+import authRouter from "./routes/authRouter.js";
+import dataRouter from "./routes/dataRouter.js"
+
+// Load variable from .env file
+dotev.config();
+connectDB();
+
 const app = express();
+
 app.use(express.json());
 app.use(cors({
     origin: ["https://vishal-kr.vercel.app/"],
@@ -8,8 +38,18 @@ app.use(cors({
     credentilas: true
 }));
 
+Routes
+app.use("/history", HistoryRoute);
+app.use("/user", authRouter);
+app.use("/home", dataRouter)
 app.get("/", (req, res) => {
-    res.status(200).json({ message: "server is running" });
+  res.status(200).json({msg: "The server is up and running"})
 })
 
-app.listen(5000, console.log("Server started on port 5000"));
+//const PORT = process.env.PORT || 5000;
+
+
+
+app.listen(5000, () => {
+  console.log(`Server running at port 5000`);
+});
