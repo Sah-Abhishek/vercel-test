655c02e366798032f88b247d67e33c194924cca3
Sah-Abhishek <abhisheksah3333@gmail.com>
2024-11-16 17:31:49 +0530

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from 'express';
 const app = express();
 app.use(express.json());
-// app.use(cors());
+app.use(cors({
+    origin: ["https://vishal-kr.vercel.app/"],
+    methods: ["POST", "GET"],
+    credentilas: true
+}));
 
 app.get("/", (req, res) => {
-    res.status(200).json({message: "server is running"});
+    res.status(200).json({ message: "server is running" });
 })
 
 app.listen(5000, console.log("Server started on port 5000"));
