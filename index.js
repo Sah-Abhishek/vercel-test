2a6faf6f76b88f4458cb7753bdf890bb3789d45a
Sah-Abhishek <abhisheksah3333@gmail.com>
2024-11-16 18:50:55 +0530

    cors updated

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: ["https://vishal-kr.vercel.app/"],
+    origin: '*',
     methods: ["POST", "GET"],
     credentilas: true
 }));
