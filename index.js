90d32b6743cc64c194fb7034e7a6b69538f53805
Sah-Abhishek <abhisheksah3333@gmail.com>
2024-11-16 18:24:56 +0530

    uini

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 // import HistoryRoute from "./routes/HistoryRoute.js";
 import authRouter from "./routes/authRouter.js";
-// import dataRouter from "./routes/dataRouter.js"
+import dataRouter from "./routes/dataRouter.js"
 
 // Load variable from .env file
 dotev.config();
@@ -41,7 +41,7 @@ app.use(cors({
 // Routes
 // app.use("/history", HistoryRoute);
 app.use("/user", authRouter);
-// app.use("/home", dataRouter)
+app.use("/home", dataRouter)
 app.get("/", (req, res) => {
   res.status(200).json({msg: "The server is up and running"})
 })
